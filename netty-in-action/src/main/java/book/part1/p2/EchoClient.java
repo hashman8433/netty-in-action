4593421a72d24f767b542348966872f415f08e13
hashman8433 <843355856@qq.com>
2017-11-03 17:57:07 +0800

    alter import

--- netty-in-action/src/main/java/book/part1/p2/EchoClient.java
+++ netty-in-action/src/main/java/book/part1/p2/EchoClient.java
@@ -1,7 +1,5 @@
 package book.part1.p2;
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.net.InetSocketAddress;
 
 
@@ -13,10 +11,6 @@ import io.netty.channel.ChannelInitializer;
 import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioSocketChannel;
-import io.netty.handler.codec.DelimiterBasedFrameDecoder;
-import io.netty.handler.codec.Delimiters;
-import io.netty.handler.codec.string.StringDecoder;
-import io.netty.handler.codec.string.StringEncoder;
 
 /**
  * ClassName:EchoClient.java
